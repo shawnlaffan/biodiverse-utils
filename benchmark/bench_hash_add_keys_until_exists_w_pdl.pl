c5a764fd0b2b765dfef28ca1341880f999bc47e3
shawnlaffan <shawnlaffan@gmail.com>
2022-09-22 17:08:13 +1000

    Benchmark script that generates paths, and will use PDL

--- benchmark/bench_hash_add_keys_until_exists_w_pdl.pl
+++ benchmark/bench_hash_add_keys_until_exists_w_pdl.pl
@@ -3,7 +3,9 @@ use Benchmark qw {:all};
 use 5.016;
 
 #use Panda::Lib qw /hash_merge/;  #  does not work on windows
-use Test::More;
+use Test2::V0;
+
+use Data::Printer;
 
 use Biodiverse::Utils::XS;
 use Biodiverse::Utils::PP;
@@ -15,67 +17,98 @@ my $prng4 = Math::Random::MT::Auto->new(seed => 222);
 
 $| = 1;
 
-my @labels = (1 .. 10000);
+#  grow a bifurcating tree
+my @names = 'a' .. 'zzzzz';
 
-my $check_count = 100;
+my @node_counts = (10, 100, 500, 1000);
+@node_counts = (100);
 
+foreach my $node_count (@node_counts) {
+    print "\nCount is $node_count\n";
 
-foreach my $count (10000, 10, 50, 100, 300, 500, 1000) {
-    $check_count = $count;
-    print "\nCheck count is $check_count\n";
+    my $paths = generate_paths ($node_count);
 
-    my @key_lists;
-    my @key_hashes;
+    my @key_lists = $prng4->shuffle ($paths);
+    p @key_lists;
 
-    for my $idx_start (0 .. 99) {
-        my @shuffled_labels = $prng4->shuffle (@labels);
-        
-        my @sublist = @shuffled_labels[0 .. $check_count];
-        push @sublist, @labels[0..$check_count];  #  make sure we have some overlap between lists
-        push @key_lists, \@sublist;
-        my %sub_hash;
-        @sub_hash{@sublist} = undef;
-        push @key_hashes, \%sub_hash;
-    }
+    my $hash_crash = hash_crash (\@key_lists);
+    my $last_if    = last_if (\@key_lists);
+    my $xs         = xs (\@key_lists);
+    my $pp         = pp (\@key_lists);
 
-    #my $panda      = panda (\@key_hashes);
-    my $grep_first = grep_first (\@key_hashes);
-    my $hash_crash = hash_crash (\@key_hashes);
-    my $next_if    = next_if (\@key_hashes);
-    my $xs         = xs (\@key_hashes);
-    my $pp         = pp (\@key_hashes);
+    is ($xs, $last_if, "xs and next_if match");
+    is ($xs, $hash_crash, "xs and hash_crash match");
+    is ($xs, $pp, "xs and pp match");
 
-    #is_deeply ($xs, $grep_first, "xs and grep_first match");
-    #is_deeply ($xs, $next_if, "xs and next_if match");
-    #is_deeply ($xs, $hash_crash, "xs and hash_crash match");
-    #is_deeply ($xs, $pp, "xs and pp match");
-
-    #printf "%12s%8d\n", "panda:", scalar keys %$panda;
-    #printf "%12s%8d\n", "grep_first:", scalar keys %$grep_first;
-    #printf "%12s%8d\n", "hash_crash:", scalar keys %$grep_first;
-    #printf "%12s%8d\n", "next_if:", scalar keys %$grep_first;
+    #printf "%12s%8d\n", "hash_crash:", scalar keys %$hash_crash;
+    #printf "%12s%8d\n", "last_if:", scalar keys %$hash_crash;
+    #printf "%12s%8d\n", "pp:", scalar keys %$pp;
+    #printf "%12s%8d\n", "xs:", scalar keys %$xs;
 
     cmpthese (
         -3,
         {
-            hash_crash => sub {hash_crash(\@key_hashes)},
-            #grep_first => sub {grep_first(\@key_hashes)},
-            #next_if    => sub {next_if(\@key_hashes)},
-            xs         => sub {xs(\@key_hashes)},
-            pp         => sub {pp(\@key_hashes)},
+            hash_crash => sub {hash_crash(\@key_lists)},
+            last_if    => sub {last_if(\@key_lists)},
+            xs         => sub {xs(\@key_lists)},
+            pp         => sub {pp(\@key_lists)},
         }
     );
 }
 
 done_testing();
 
+sub generate_paths {
+    my $n = shift // 2000;
+
+    my %tree;
+    my %parent_hash = ($names[1] => $names[0]);
+    my @children = (1);
+    while (defined (my $i = shift @children)) {
+        my @current = ($i * 2, $i * 2 + 1);
+        push @children, @current;
+        $parent_hash{$names[$current[0]]} = $names[$i];
+        $parent_hash{$names[$current[1]]} = $names[$i];
+        $tree{$names[$i]} = [@names[@current]];
+        $tree{$names[$current[0]]} = [];
+        $tree{$names[$current[1]]} = [];
+        #say join ' ', @current;
+        last if keys %parent_hash >= $n;
+    }
+    
+    say 'Tree generated';
+    
+    use Data::Printer;
+    #p %tree;
+    #p %parent_hash;
+    my @terminals = sort grep {scalar @{$tree{$_}} == 0} keys %tree;
+    #p @terminals;
+    
+    my @paths_to_root;
+    foreach my $terminal (@terminals) {
+        my @path = ($terminal);
+        my $parent = $parent_hash{$terminal};
+        while ($parent) {
+           push @path, $parent;
+           $parent = $parent_hash{$parent};
+        }
+        push @paths_to_root, \@path;
+    }
+    #p @paths_to_root;
+    say "Generated " . scalar @paths_to_root . " paths";
+
+    return (\@paths_to_root);
+}
+
+
 sub xs {
     my $key_lists = shift;
     
     my %path;
+    #@path{@{$key_lists->[0]}} = ();
 
     for my $sub_list (@$key_lists) {
-        Biodiverse::Utils::XS::add_hash_keys (\%path, $sub_list);
+        Biodiverse::Utils::XS::add_hash_keys_until_exists (\%path, $sub_list);
     }
     
     return \%path;
@@ -87,7 +120,7 @@ sub pp {
     my %path;
 
     for my $sub_list (@$key_lists) {
-        Biodiverse::Utils::PP::add_hash_keys (\%path, $sub_list);
+        Biodiverse::Utils::PP::add_hash_keys_until_exists (\%path, $sub_list);
     }
     
     return \%path;
@@ -99,33 +132,20 @@ sub hash_crash {
     my %path;
 
     for my $sub_list (@$key_lists) {
-        @path{keys %$sub_list} = undef;
+        @path{@$sub_list} = ();
     }
     
     return \%path;
 }
 
-
-sub grep_first {
+sub last_if {
     my $key_lists = shift;
 
     my %path;
 
     for my $sub_list (@$key_lists) {
-        @path{grep {!exists $path{$_}} keys %$sub_list} = undef;
-    }
-    
-    return \%path;
-}
-
-sub next_if {
-    my $key_lists = shift;
-
-    my %path;
-
-    for my $sub_list (@$key_lists) {
-        for my $label (keys %$sub_list) {
-            next if exists $path{$label};
+        for my $label (@$sub_list) {
+            last if exists $path{$label};
             $path{$label} = undef;
         }
     }
